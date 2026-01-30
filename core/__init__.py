a8204b97168b9936ca0ae73219afa6642b9b1268
Kaushal <kaushal.beyblader@gmail.com>
2026-01-29 21:25:45 -0500

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,12 +1,12 @@
 """Core package for the AI agent engine."""
 
-from .config import settings
+from . import config
 from .engine import BaseContext, ReActContext
 from .responses import BaseResponse, ReActResponse, Message
 from .tools import execute_command, get_mcp_toolkit
 
 __all__ = [
-    "settings",
+    "config",
     "BaseResponse",
     "ReActResponse",
     "Message",
